4458989d9efda799f0a53ab0ab03f7ccf78006b6
scutzck033 <404762007@qq.com>
2017-03-14 13:42:56 +0800

    add 4244_conv_comparsion

--- conv_comparision.py
+++ conv_comparision.py
@@ -1,5 +1,4 @@
-#4*24*1 predict 24*1
-#using benchmark MLP
+#4*24*4 predict 24*1
 
 from __future__ import print_function
 import numpy as np
@@ -15,22 +14,32 @@ import h5py
 from keras.models import model_from_json
 
 
+# Convolution
+nb_row=3
+nb_col=3
+nb_filter = 64
+pool_size = 2
+
+# LSTM
+lstm_output_size = 64
+
 # Training
 batch_size = 32
 nb_epoch = 100
 
 n_frames=4
 n_hours=4
-n_cols=1
+n_cols=4
 
 #Normalization
 def MaxMinNormalization(x,Max,Min):
     x = (x - Min) / (Max - Min);
     return x;
 
+
 #load training raw data
 rawdata_train = pd.read_csv("4h per day(1h).csv",encoding='gbk')
-rawdata_train=rawdata_train[[2]] # open,close,highest,lowest
+rawdata_train=rawdata_train[[2,3,4,5]] # open,max,min,close
 
 
 data=rawdata_train.as_matrix()[0:160]
@@ -39,9 +48,12 @@ temp_dataX= []
 temp_dataY= []
 
 
-
 temp_dataX=data[0:(data.shape[0]-n_hours)]
 temp_dataY=data[n_frames*n_hours:data.shape[0]]
+
+#get one column
+temp_dataY=np.transpose(np.transpose(temp_dataY)[0:1])
+
 temp_dataX=np.reshape(temp_dataX,-1)
 temp_dataY=np.reshape(temp_dataY,-1)
 
@@ -49,19 +61,15 @@ temp_dataY=np.reshape(temp_dataY,-1)
 
 dataX =[]
 dataY= []
-# here is for the case that input graph and output graph
+
+
+# here is for the case that input graph and output one col
 for i in range(temp_dataX.shape[0]-n_frames*n_hours*n_cols+n_hours*n_cols):
     if i%(n_hours*n_cols) == 0:
         dataX.append(temp_dataX[i:i+n_frames*n_hours*n_cols])
+for i in range(temp_dataY.shape[0]):
+    if i%(n_hours*1)==0:
         dataY.append(temp_dataY[i])
-
-
-
-
-
-
-dataX=np.reshape(dataX,(np.array(dataX).shape[0],-1))
-dataY=np.reshape(dataY,(np.array(dataY).shape[0],-1))
 
 #Normalization
 # maxV=np.max(dataY)
@@ -70,16 +78,27 @@ dataY=np.reshape(dataY,(np.array(dataY).shape[0],-1))
 #     for j in range(np.array(dataY).shape[1]):
 #         dataY[i][j]=MaxMinNormalization(dataY[i][j],maxV,minV)
 
-print (dataY)
+
+
+dataX=np.reshape(dataX,(np.array(dataX).shape[0],n_frames*n_hours,n_cols,1))
+dataY=np.reshape(dataY,(np.array(dataY).shape[0],-1))
+
+print (dataX.shape)
+print (dataY.shape)
+
+
 #build the model
 model = Sequential()
-model.add(Dense(output_dim=64, input_dim=n_frames*n_hours*n_cols))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=1))
-# model.add(Activation("sigmoid"))
+model.add(Convolution2D(nb_filter, nb_row, nb_col, border_mode='same',input_shape=(np.array(dataX).shape[1],np.array(dataX).shape[2],np.array(dataX).shape[3])))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size), border_mode='same'))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(np.array(dataY).shape[1]))
+
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-plot(model,to_file='model_mlp_4241.png',show_shapes=True)
+
+plot(model,to_file='model_4244_conv.png',show_shapes=True)
 
 print('Train...')
 model.fit(dataX, dataY, batch_size=batch_size, nb_epoch=nb_epoch,verbose=0)
@@ -87,5 +106,5 @@ model.fit(dataX, dataY, batch_size=batch_size, nb_epoch=nb_epoch,verbose=0)
 
 
 json_string = model.to_json()
-open('model_mlp_4241_architecture.json','w').write(json_string)
-model.save_weights('model_mlp_4241_weights.h5')
+open('model_4244_conv_architecture.json','w').write(json_string)
+model.save_weights('model_4244_conv_weights.h5')
--- model_test.py
+++ model_test.py
@@ -67,6 +67,67 @@ def model_mlp_4241_test(rawdata):
     error = temp / (prediction.shape[0] * prediction.shape[1])
     print("Model_mlp_4241 error: %.2f%%" % (error * 100))
 
+def model_conv_4244_test(rawdata):
+    rawdata_test = rawdata[[2,3,4,5]]
+    dataTest = rawdata_test.as_matrix()[160:244]  # [0:888]
+
+    temp_dataX_Test = []
+    temp_dataY_Test = []
+
+    n_frames = 4
+    n_hours = 4
+    n_cols = 4
+
+    temp_dataX_Test = dataTest[0:(dataTest.shape[0] - n_hours)]
+    temp_dataY_Test = dataTest[n_frames * n_hours:dataTest.shape[0]]
+
+    # get one column
+    temp_dataY_Test= np.transpose(np.transpose(temp_dataY_Test)[0:1])
+
+    temp_dataX_Test = np.reshape(temp_dataX_Test, -1)
+    temp_dataY_Test = np.reshape(temp_dataY_Test, -1)
+
+    dataX_Test = []
+    dataY_Test = []
+
+    for i in range(temp_dataX_Test.shape[0] - n_frames * n_hours * n_cols + n_hours * n_cols):
+        if i % (n_hours * n_cols) == 0:
+            dataX_Test.append(temp_dataX_Test[i:i + n_frames * n_hours * n_cols])
+    for i in range(temp_dataY_Test.shape[0]):
+        if i % (n_hours * 1) == 0:
+            dataY_Test.append(temp_dataY_Test[i])
+
+    dataX_Test = np.reshape(dataX_Test, (np.array(dataX_Test).shape[0], n_frames * n_hours, n_cols, 1))
+    dataY_Test = np.reshape(dataY_Test, (np.array(dataY_Test).shape[0], -1))
+
+    # Normalization
+    # maxV = np.max(dataY_Test)
+    # minV = np.min(dataY_Test)
+    # for i in range(np.array(dataY_Test).shape[0]):
+    #     for j in range(np.array(dataY_Test).shape[1]):
+    #         dataY_Test[i][j] = MaxMinNormalization(dataY_Test[i][j], maxV, minV)
+
+    model = model_from_json(open('model_4244_conv_architecture.json').read())
+    model.load_weights('model_4244_conv_weights.h5')
+
+
+
+    print('Score...')
+    prediction = model.predict(dataX_Test, verbose=0)
+    print('Predict...')
+
+    temp = 0.0
+    print(prediction)
+
+    print('label...')
+    print(dataY_Test)
+
+    for i in range(prediction.shape[0]):
+        for j in range(prediction.shape[1]):
+            temp = temp + abs(prediction[i][j] - dataY_Test[i][j]) / dataY_Test[i][j]
+    error = temp / (prediction.shape[0] * prediction.shape[1])
+    print("Model_4244_conv error: %.2f%%" % (error * 100))
+
 
 def model_merge_test(rawdata):
     rawdata_test = rawdata[[2]]
@@ -248,6 +309,7 @@ rawdata_test = pd.read_csv("4h per day(1h).csv",encoding='gbk')
 # model_4241_test(rawdata_test)
 model_mlp_4241_test(rawdata_test)
 # model_merge_test(rawdata_test)
+model_conv_4244_test(rawdata_test)
 
 
 
